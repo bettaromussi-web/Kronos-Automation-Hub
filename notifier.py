f426421ed629b91d623848a45be130793ee45adb
bettaromussi-web <betta.romussi@gmail.com>
2026-05-09 15:18:36 +0200

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -2,8 +2,8 @@ import requests
 import os
 
 def send_telegram_msg(message):
-    token = os.getenv('TELEGRAM_TOKEN')
-    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    token = os.getenv('8640695510:AAGohMq0xkYrLayTv33MvzYtVFPDU1DLqKI')
+    chat_id = os.getenv('696552391')
     url = f"https://telegram.org{token}/sendMessage"
     payload = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
     requests.post(url, data=payload)
